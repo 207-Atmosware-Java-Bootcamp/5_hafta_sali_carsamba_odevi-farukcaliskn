0456d0e3186b58c579d5bd73eeb5d2d1fe3f8106
farukcaliskn <faruk.caliskn@outlook.com>
2022-06-23 17:15:37 +0300

    revize

--- Odevler/src/_1_VizeFinalHesaplama.java
+++ Odevler/src/_1_VizeFinalHesaplama.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class Odev1 {
+public class _1_VizeFinalHesaplama {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Vize notunuzu giriniz : ");
 		double vizeNot = scanner.nextDouble();
 		System.out.print("Final notunuzu giriniz : ");
 		double finalNot = scanner.nextDouble();
-		int ortalama = (int) ((vizeNot*0.4) + (finalNot*0.6));
-		System.out.println(ortalama);
+		int ortalama = (int) ((vizeNot * 0.4) + (finalNot * 0.6));
 		if (ortalama < 50) {
 			System.out.println("Bu Dersten Kaldınız");
 		} else if (ortalama == 50) {
 			System.out.println("Bu Dersten Geçtiniz");
-		} else if (55 <= ortalama && ortalama <= 70) {
+		} else if (51 <= ortalama && ortalama <= 70) {
 			System.out.println("Bu Dersten BB İle Geçtiniz");
 		} else if (70 <= ortalama && ortalama <= 84) {
 			System.out.println("Bu Dersten BA İle Geçtiniz");
